5cadac3bf82298ce39fdd9113c4a871cad05c7b6
Victória Valderano <vickvalderano@gmail.com>
2025-11-11 16:26:09 -0300

    Trocando cores app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ st.header('Anúncios de vendas de Carros')
 hist_button = st.button('Criar histograma')
 if hist_button:
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data, x="odometer"
+                      color_discrete_sequence=["#FFB6C1"])
     st.plotly_chart(fig, use_container_width=True)
     fig.show()
 
@@ -17,4 +18,6 @@ scatter_button = st.button('Criar Dispersão')
 if scatter_button:
     st.write('Criando um Dispersão para o conjunto de dados de anúncios de vendas de carros')
     fig = px.scatter(car_data, x="odometer", y="price")
+    fig.update_traces(marker=dict(color="#FFB6C1"))
+    st.plotly_chart(fig, use_container_width=True)
     fig.show()
